8eb0e1e0c9170670ab590cc831d21165e4f4670a
HarrisonKaufman <45081623+HarrisonKaufman@users.noreply.github.com>
2021-04-27 12:33:43 -0600

    comments/formatting for gui

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -5,8 +5,6 @@ import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener {
 
-
-
     private String title = "Tuner";
     private String buttonText = "Click to Tune";
 
@@ -27,35 +25,31 @@ public class GUI implements ActionListener {
         button = new JButton(buttonText);
         buttonText = "Click to Tune";
 
-            //panel setup
-            panel.setBorder(BorderFactory.createEmptyBorder(300,300,100,100));
-            panel.setLayout(new GridLayout(0,1));
-            panel.setBackground(Color.CYAN);
-            panel.add(button);
-
+        //panel setup
+        panel.setBorder(BorderFactory.createEmptyBorder(300,300,100,100));
+        panel.setLayout(new GridLayout(0,1));
+        panel.setBackground(Color.CYAN);
+        panel.add(button);
 
         //frame setup
-            frame.add(panel, BorderLayout.CENTER);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setBackground(Color.CYAN);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setBackground(Color.CYAN);
+        
+        //make button listen for clicks
+        button.addActionListener(this);
 
-
-            button.addActionListener(this);
-
-            //make everything visible
-            panel.setVisible(true);
-            button.setVisible(true);
-            frame.setVisible(true);
-
-
-
+        //make everything visible
+        panel.setVisible(true);
+        button.setVisible(true);
+        frame.setVisible(true);
+        
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        //todo code to start microphone listener
-
+        //starts listening
         new AudioListener();
     }
 
